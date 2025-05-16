aa003587db5718462ec71ed2d1bd3e9104bf830b
이재윤 <35904239+JaeyoonLeev@users.noreply.github.com>
2025-05-16 17:05:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import crawler
 import crawledDataDownload
+import virusTotalHash
 import gpt_report
 
 
 if __name__=="__main__":
+    mainroot = '/opt/isolation'
     print("키워드를 입력하세요 : ")
+
     keyword = input()
 
     print(keyword)
     print(type(keyword))
 
-    #1차    
+    # #1차    
     if keyword == '':
         crawler.pageSelenium(keyword)
     else:
@@ -18,11 +21,11 @@ if __name__=="__main__":
         crawler.pageSelenium(keyword)
 
     crawler.crawledPageDataInsert()
-    gpt_report.run_pipeline(keyword)
 
-    #2차
-    #crawledDataDownload.main()
-    
+    # #2차
+    crawledDataDownload.main(mainroot)
+    virusTotalHash.scan_all_files_in_directory(mainroot)
+    gpt_report.run_pipeline(keyword)
 
     #3차
 
